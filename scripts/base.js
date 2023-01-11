be535e84b31c13802116c724f2a4904fe287e7db
mmusih <hub22001@byui.edu>
2023-01-11 15:18:19 +0200

    week1

---
+++ scripts/base.js
@@ -0,0 +1,6 @@
+let curretDate = new Date();
+const Year = curretDate.getFullYear();
+document.getElementById("year").textContent = Year;
+
+time = document.lastModified;
+document.querySelector("#currentdate").textContent = time;
